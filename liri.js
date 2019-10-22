f538bc63f6250fe1d566d96c7ab4c416bda0ee8b
Reducks <reducks3000@gmail.com>
2019-10-22 00:24:24 -0500

    added variables to liri.js

--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
 require("dotenv").config();
+const keys = require("./keys.js")
 
-let keys = require("./keys.js");
+const axios = require('axios')
+const moment = require('moment')
+let Spotify = require('node-spotify-api')
+let fs = require("fs");
+
+let spotify = new Spotify(keys.spotify)
